ce321dbdd5379c731cc49b6b4faa9a1b52556ca0
Arvind Kumar <arvindkumar2575@gmail.com>
2022-09-30 22:50:45 +0530

    initial commit

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/ci-practice/';
+    public $baseURL = 'http://localhost:8080/ci-student-management-system/';
 
     /**
      * --------------------------------------------------------------------------
